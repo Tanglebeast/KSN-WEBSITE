e61ea8120e5c31a2f8360ad847d0e50695ceca56
Tanglebeast <marvguellier1999@gmail.com>
2024-11-07 22:46:15 +0100

    v3

--- assets/js/partners-carousel.js
+++ assets/js/partners-carousel.js
@@ -1,37 +1,23 @@
-// assets/js/partners-carousel.js
-
 $(document).ready(function () {
-    $(".bp-slider").slick({
-      slidesToShow: 6,           // Anzahl der sichtbaren Slides
-      slidesToScroll: 1,         // Anzahl der Slides, die beim Scrollen bewegt werden
-      autoplay: true,            // Autoplay aktivieren
-      autoplaySpeed: 1500,       // Geschwindigkeit des Autoplays in Millisekunden
-      arrows: false,             // Pfeile deaktivieren
-      dots: false,               // Punkte deaktivieren
-      pauseOnHover: false,       // Autoplay beim Hover nicht anhalten
+    $(".customer-logos").slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
       responsive: [
         {
-          breakpoint: 992,        // Ab einer Breite von 992px
-          settings: {
-            slidesToShow: 5
-          }
-        },
-        {
-          breakpoint: 768,        // Ab einer Breite von 768px
+          breakpoint: 768,
           settings: {
             slidesToShow: 4
           }
         },
         {
-          breakpoint: 576,        // Ab einer Breite von 576px
+          breakpoint: 520,
           settings: {
             slidesToShow: 3
-          }
-        },
-        {
-          breakpoint: 480,        // Ab einer Breite von 480px
-          settings: {
-            slidesToShow: 2
           }
         }
       ]
